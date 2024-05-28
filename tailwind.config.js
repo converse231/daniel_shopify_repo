98a64a87d376de6cf9296a862b03d234d03cf7b6
converse231 <paragasdaniel23@gmail.com>
2024-05-28 15:25:25 +0800

    Hello Shopify

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/customers/*.liqud",
+    "./templates/*.liquid",
+    "./assets/*.liquid",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
